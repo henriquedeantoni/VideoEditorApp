79190cb9378a7cf4273209cf635374304703c73b
henriquedeantoni <henriqdeantoni@gmail.com>
2025-04-07 13:39:47 -0300

    first commit

---
+++ Core/Models/VideoEditOptionsConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models;
+
+public class VideoEditOptionsConfiguration
+{
+    public string InputPath { get; set; } = string.Empty;
+    public string OutputPath { get; set; } = string.Empty;
+
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+
+    public bool MirrorHorizontal { get; set; }
+    public bool MirrorVertical { get; set; }
+
+    public float RotationAngle { get; set; } // Degrees clockwise
+
+    public int? CropXStart { get; set; }
+    public int? CropXEnd { get; set; }
+    public int? CropYStart { get; set; }
+    public int? CropYEnd { get; set; }
+}
---
+++ Infrastructure/FfmpegService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Core.Models;
+
+namespace Infrastructure;
+
+public class FfmpegService
+{
+    public string GenerateCommand(VideoEditOptionsConfiguration options)
+    {
+        var filters = new List<string>();
+
+        if (options.MirrorHorizontal) filters.Add("hflip");
+        if (options.MirrorVertical) filters.Add("vflip");
+
+        if (options.RotationAngle != 0)
+        {
+            float angle = options.RotationAngle * (float)Math.PI / 180f;
+            filters.Add($"rotate={angle}:ow=rotw({angle}):oh=roth({angle}):c=black@0");
+        }
+
+        if (options.CropXStart.HasValue && options.CropXEnd.HasValue && options.CropYStart.HasValue && options.CropYEnd.HasValue)
+        {
+            int width = options.CropXEnd.Value - options.CropXStart.Value;
+            int height = options.CropYEnd.Value - options.CropYStart.Value;
+            filters.Add($"crop={width}:{height}:{options.CropXStart}:{options.CropYStart}");
+        }
+
+        string filterString = filters.Count > 0 ? $" -vf \"{string.Join(",", filters)}\"" : string.Empty;
+
+        return $"-i \"{options.InputPath}\" -ss {options.StartTime} -to {options.EndTime} {filterString} -y \"{options.OutputPath}\"";
+    }
+
+    public void RunFfmpegCommand(string arguments)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+        process.Start();
+        process.WaitForExit();
+    }
+}
