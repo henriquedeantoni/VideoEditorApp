cd0088162507744e0c51c8a6028e32773c833566
henriquedeantoni <henriqdeantoni@gmail.com>
2025-04-08 22:59:02 -0300

    feat: created new features elements in forms, create preview screen

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -2,9 +2,15 @@ namespace WinForms
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
