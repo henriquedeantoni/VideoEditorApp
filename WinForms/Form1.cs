804a46fb2acce876333526297b0d6fcaa9e8bfc3
henriquedeantoni <henriqdeantoni@gmail.com>
2025-04-11 13:47:59 -0300

    feat: new labels created, icon asstes added

--- WinForms/Form1.cs
+++ WinForms/Form1.cs
@@ -1,21 +1,27 @@
-namespace WinForms
+namespace WinForms;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    static string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "closeIcon.png");
+
+    public Form1()
+    {
+        InitializeComponent();
+        btnPathSource.Image = Image.FromFile(imagePath);
+    }
+
+    private void checkBox2_CheckedChanged(object sender, EventArgs e)
     {
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
+    }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
+    private void lblSource_Click(object sender, EventArgs e)
+    {
 
-        }
+    }
 
-        private void lblSource_Click(object sender, EventArgs e)
-        {
+    private void Form1_Load(object sender, EventArgs e)
+    {
 
-        }
     }
 }
